8ed667b4a8ae1fa42301ee0dfc0a466b6559114a
maxwellwebber <maxweb@uvic.ca>
2015-11-03 14:19:54 -0800

    Added EnterRoomDescriptions to items

--- gamesite/static/js/item.js
+++ gamesite/static/js/item.js
@@ -1,26 +1,27 @@
-var Item = function(room, name, description, useMessage) {
+var Item = function(room, name, description, enterRoomDescription, useMessage) {
 	/*strings*/
 	this.name = name;
 	this.room = room;
 	this.description = description;
+	this.enterRoomDescription = enterRoomDescription;
 	this.useMessage = useMessage;
 	/*adds a new item into the room array when the item is created*/
 	room.itemsInRoom.push(this);
 }
 
-var Pickupable = function(room, name, description, useMessage) {
-	Item.call(this, room, name, description, useMessage);
+var Pickupable = function(room, name, description, enterRoomDescription, useMessage) {
+	Item.call(this, room, name, description, enterRoomDescription, useMessage);
 }
 
-var NonPickupable = function(room, name, description, useMessage) {
-	Item.call(this, room, name, description, useMessage);
+var NonPickupable = function(room, name, description, enterRoomDescription, useMessage) {
+	Item.call(this, room, name, description, enterRoomDescription, useMessage);
 }
 
 /*this item can be picked up and used if it is picked up, it's usePattern is just a regular
 expression that is matched for the items use command.  it's needed so different items can
 be used by typing different verbs.*/
-var PickupableAndUsable = function(room, name, description, useMessage, usePattern) {
-	Pickupable.call(this, room, name, description, useMessage);
+var PickupableAndUsable = function(room, name, description, enterRoomDescription, useMessage, usePattern) {
+	Pickupable.call(this, room, name, description, enterRoomDescription, useMessage);
 	this.usePattern = usePattern;
 	this.inInv = false;
 }
@@ -28,21 +29,21 @@ var PickupableAndUsable = function(room, name, description, useMessage, usePatte
 /*this item cannot be picked up but it can be used, it's usePattern is just a regular
 expression that is matched for the items use command.  it's needed so different items can
 be used by typing different verbs.*/
-var NonPickupableAndUsable = function(room, name, description, useMessage, usePattern) {
-	NonPickupable.call(this, room, name, description, useMessage);
+var NonPickupableAndUsable = function(room, name, description, enterRoomDescription, useMessage, usePattern) {
+	NonPickupable.call(this, room, name, description, enterRoomDescription, useMessage);
 	this.usePattern = usePattern;
 }
 
 /*this item can be examined but it can't be used or picked up*/
-var Decoration = function(room, name, description, useMessage) {
-	NonPickupable.call(this, room, name, description, useMessage);
+var Decoration = function(room, name, description, enterRoomDescription, useMessage) {
+	NonPickupable.call(this, room, name, description, enterRoomDescription, useMessage);
 }
 
 /*the main difference between a door and a NonPickupableAndUsable is that doors need
 to have both rooms they connect to passed in them, they also have a boolean to say 
 if they're locked or not*/
-var Door = function(room, room2, name, description, useMessage, locked) {
-	NonPickupable.call(this, room, name, description, useMessage);
+var Door = function(room, room2, name, description, enterRoomDescription, useMessage, locked) {
+	NonPickupable.call(this, room, name, description, enterRoomDescription, useMessage);
 	this.locked = locked;
 	this.room2 = room2;
 	room2.itemsInRoom.push(this);
--- gamesite/static/js/script.js
+++ gamesite/static/js/script.js
@@ -12,13 +12,13 @@ $(function()  {
 
 	/*items*/
 	// need to make it so keys are linked to doors in some way
-	key1 = new PickupableAndUsable(room1,"key1", "Unlocks somthing.", "You need to use it on somthing.", pattUseItem);
-	key2 = new PickupableAndUsable(room2,"key2", "Unlocks somthing.", "You need to use it on somthing.", pattUseItem);
+	key1 = new PickupableAndUsable(room1,"key1", "Unlocks somthing.", "there is a key on the ground (called 'key1'), ", "You need to use it on somthing.", pattUseItem);
+	key2 = new PickupableAndUsable(room2,"key2", "Unlocks somthing.", "there is a key on the ground (called 'key2'), ", "You need to use it on somthing.", pattUseItem);
 
-	door1 = new Door(room1, room2, "door", "its shut.", "You try to open the door but it's locked.", true);
-	door2 = new Door(room2, room3, "door2", "its shut.", "You try to open the door but it's locked.", true);
-	clock = new Decoration(room1,"clock", "It's a clock, It appears to be broken.", null);
-	gun = new PickupableAndUsable(room1,"gun", "It doesn't appear to be loaded.", "You try to fire the gun but you can't.", pattShootItem);
+	door1 = new Door(room1, room2, "door", "its shut.", "there is a door on the south wall, ", "You try to open the door but it's locked.", true);
+	door2 = new Door(room2, room3, "door2", "its shut.", "there is a door on the east wall, ", "You try to open the door but it's locked.", true);
+	clock = new Decoration(room1,"clock", "It's a clock, It appears to be broken.", "there is a clock on the wall, ", null);
+	gun = new PickupableAndUsable(room1,"gun", "It doesn't appear to be loaded.", "there is a gun on the floor, ", "You try to fire the gun but you can't.", pattShootItem);
 
 	/*this array will be [north,east,south,west] null means no door
 	might make this automatically happen in the constructor later*/
