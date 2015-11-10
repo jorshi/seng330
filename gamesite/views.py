a6d98219b920e0399a27e9ac7ca81a97da74eb46
jorshi <jordieshier@gmail.com>
2015-11-10 01:47:16 -0800

    Django serves Doxygen

--- gamesite/views.py
+++ gamesite/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import django.contrib.staticfiles.views as static_server
 
-def doxygen(request):
+def doxygen(request, path=None):
+    """
+    Hack to serve doxygen files
+    """
 
-   pass 
+    if not path:
+        return static_server.serve(request, "doxygen/html/index.html")
+    # Super hacky ... for some reason index.html doesn't work ??
+    elif path == "index.html/":
+        return redirect("/doxygen/")
+    else:
+        return static_server.serve(request, "doxygen/html/%s" % path)
