ebfb76838eda199f6ef617154caa2d5da9721f4e
jorshi <jordieshier@gmail.com>
2015-11-10 01:47:16 -0800

    Django serves Doxygen

--- gamesite/urls.py
+++ gamesite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import player.views
+import gamesite.views
+import gamesite.settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -9,5 +11,12 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', player.views.login_register, {'tab': 'login'}), # use with @login_required
     url(r'^logout/$', player.views.player_logout, name='logout'),
     url(r'^register/$', player.views.login_register, {'tab': 'register'}, name='register'),
-    
+
 )
+
+# Serve Doxygen static files in development #TODO look into serving for production
+if gamesite.settings.DEBUG:
+    urlpatterns += [
+        url(r'^doxygen/(?P<path>.*)$',
+        gamesite.views.doxygen,
+    )]
