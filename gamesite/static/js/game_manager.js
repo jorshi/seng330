ae96d5f32111262f159b216d372160fcca5ebe7f
jorshi <jordieshier@gmail.com>
2015-11-29 01:29:45 -0800

    Updated the game manager javascript to reflect changes to the API

--- gamesite/static/js/game_manager.js
+++ gamesite/static/js/game_manager.js
@@ -7,7 +7,7 @@
  */
 
 var GameManager = function() {
-    
+
     // Attributes
     self = this; // store reference to 'this'
     this.room = null;
@@ -27,8 +27,18 @@ var GameManager = function() {
     // Make request to get the inventory for the current room
     this.get_room_inventory = function() {
         if (this.room !== null) {
-            $.get('/get_room_inventory/?room=' + this.room.pk, function(data) {
+            $.get('/get_inventory/?room=' + this.room.pk, function(data) {
                 self.load_inventory(data);
+            });
+        }
+    };
+
+
+    // Make request to get the doors for the current room
+    this.get_room_doors = function() {
+        if (this.room !== null) {
+            $.get('/get_doors/?room=' + this.room.pk, function(data) {
+                self.load_doors(data);
             });
         }
     };
@@ -45,6 +55,7 @@ var GameManager = function() {
         try {
             this.room = data[0];
             this.get_room_inventory();
+            this.get_room_doors();
         } catch(err) {
             console.log("Failed to load map! : " + err);
         }
@@ -54,8 +65,15 @@ var GameManager = function() {
     // Callback for the get room inventory request
     this.load_inventory = function(data) {
         console.log("Got Inventory!");
-        console.log(data);
+
+        // Do stuff with the item inventory for this room here
     };
 
 
+    // Callback for the get room doors request
+    this.load_doors = function(data) {
+        console.log("Got Doors!");
+
+        // Do stuff with the doors for this room here
+    };
 };
